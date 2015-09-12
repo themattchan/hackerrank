d747f02487b7010e754210b287a62eb764aa97ba
Matthew Chan <matt@themattchan.com>
2015-09-12 13:28:42 -0700

    monads <3

--- algo/sort/Quicksort.hs
+++ algo/sort/Quicksort.hs
@@ -1,6 +1,7 @@
 module Quicksort where
 import Data.List
 import Control.Applicative
+import Control.Monad
 
 displayList :: [Int] -> String
 displayList = intercalate " " . map show
@@ -10,9 +11,21 @@ qs1 :: [Int] -> [Int]
 qs1 (x:xs) = let (sm, gt) = partition (<= x) xs in
   sm ++ [x] ++ gt
 
+-- part 2
+quicksort :: [Int] -> IO [Int]
+quicksort []     = return []
+quicksort (x:xs) = let (sm, gt) = partition (<= x) xs in
+  do
+    ss <- quicksort sm
+    sg <- quicksort gt
+    let sorted' = ss ++ [x] ++ sg
+    when (length sorted' > 1) (putStrLn (displayList sorted'))
+    return sorted'
+
 main :: IO ()
 main = do
   getLine -- # of ns not needed
   ns <- getLine >>= mapM (\x -> pure ((read x)::Int)) . words
-  let sorted = qs1 ns
-  putStrLn $ displayList sorted
+  quicksort ns
+  return ()
+  --putStrLn $ displayList sorted
