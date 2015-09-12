2ab569ff75674047fdf145f8111753ad5a62619c
Matthew Chan <matt@themattchan.com>
2015-09-12 14:56:27 -0700

    counting sort 3

--- algo/sort/counting_sort.c
+++ algo/sort/counting_sort.c
@@ -21,17 +21,28 @@ int main(void) {
 	for (int i = 0; i < size; i++) {
 		int j = 0;
 		scanf("%d", &j);
+
+		char ss[10] = "";
+		scanf("%s", &ss);
+
 		ar[j]++;
 	}
 
 	/* #1 */
-	print_array(ar, 100);
+//	print_array(ar, 100);
 
 	/* #2 */
+	//for (int i = 0; i < 100; i++) {
+	//	for (int j = 0; j < ar[i]; j++) {
+	//		printf("%d ", i);
+	//	}
+	//}
+
+	/* #3 */
+	int running = 0;
 	for (int i = 0; i < 100; i++) {
-		for (int j = 0; j < ar[i]; j++) {
-			printf("%d ", i);
-		}
+		running += ar[i];
+		printf("%d ", running);
 	}
 
 	return 0;
