9bf961675f1ebe62ed0ecd5caa590e76ae120ac0
Matthew Chan <matt@themattchan.com>
2015-09-12 14:39:26 -0700

    counting sort

---
+++ algo/sort/countingSort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void print_array(int* ar, int size)
+{
+	for (int i = 0; i < size; i++) {
+		printf("%d ", ar[i]);
+	}
+	putchar('\n');
+}
+
+int main(void) {
+	int size;
+	scanf("%d", &size);
+
+	int ar[100];
+	for (int i = 0; i < 100; i++) {
+		ar[i] = 0;
+	}
+	for (int i = 0; i < size; i++) {
+		int j = 0;
+		scanf("%d", &j);
+		ar[j]++;
+	}
+
+	/* #1 */
+	/* print_array(ar,100); */
+
+    /* #2 */
+	/* for (int i = 0; i < 100; i++) {
+	 * 	for (int j = 0; j < ar[i]; j++) {
+	 * 		printf("%d ", i);
+	 * 	}
+	 * } */
+
+	return 0;
+}
