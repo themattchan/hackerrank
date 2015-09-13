35ad61d247b5c76f91e50beffb57d1c192222c61
Matthew Chan <matt@themattchan.com>
2015-09-13 02:23:05 -0700

    formatting

--- algo/bits/lonely.rkt
+++ algo/bits/lonely.rkt
@@ -4,9 +4,11 @@
   (sort (for/list ([i (read)])
           (read)) <))
 
-(car (foldl (lambda (x a)
+(car (foldl
+      (lambda (x a)
         (cond
           [(null? a) (cons x a)]
           [(= x (car a)) (cdr a)]
           [true (cons x a)]))
-       '() nums))
+      '()
+      nums))
