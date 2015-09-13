4c918b17b212fec0b6489adb6a2cd8d73f06687f
Matthew Chan <matt@themattchan.com>
2015-09-12 22:57:25 -0700

    use the c preprocessor, doh

--- algo/sort/counting_sort.c
+++ algo/sort/counting_sort.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define PROBLEM 4
+
 void print_array(int* ar, int size)
 {
 	for (int i = 0; i < size; i++) {
@@ -12,6 +14,8 @@ void print_array(int* ar, int size)
 }
 
 int main(void) {
+#ifdef PROBLEM
+
 	int size;
 	scanf("%d", &size);
 
@@ -22,28 +26,34 @@ int main(void) {
 		int j = 0;
 		scanf("%d", &j);
 
+#if (PROBLEM == 3)
 		char ss[10] = "";
 		scanf("%s", &ss);
+#endif
 
 		ar[j]++;
 	}
 
-	/* #1 */
-//	print_array(ar, 100);
+#if (PROBLEM == 1)
+	print_array(ar, 100);
+#endif
 
-	/* #2 */
-	//for (int i = 0; i < 100; i++) {
-	//	for (int j = 0; j < ar[i]; j++) {
-	//		printf("%d ", i);
-	//	}
-	//}
+#if (PROBLEM == 2)
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j < ar[i]; j++) {
+			printf("%d ", i);
+		}
+	}
+#endif
 
-	/* #3 */
+#if (PROBLEM == 3)
 	int running = 0;
 	for (int i = 0; i < 100; i++) {
 		running += ar[i];
 		printf("%d ", running);
 	}
+#endif
 
+#endif
 	return 0;
 }
