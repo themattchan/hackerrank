57635f20d8d3d1652641b921a6d516681c6160b3
Matthew Chan <matt@themattchan.com>
2015-09-15 01:09:33 -0700

    klfhadjskldfhakls

---
+++ algo/implementation/angry-prof.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(for ((i (read)))
+  (let* ((n (read))
+        (k (read))
+        (times (for/list ((_ n)) (read))))
+    (if (> k (length (filter (compose not positive?) times)))
+        (displayln "YES")
+        (displayln "NO"))))
