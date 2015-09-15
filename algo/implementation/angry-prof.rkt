71dc6fcd67115f8b85f923cbedb092491b1d6916
Matthew Chan <matt@themattchan.com>
2015-09-15 01:12:20 -0700

    klfhadjskldfhakls

--- algo/implementation/angry-prof.rkt
+++ algo/implementation/angry-prof.rkt
@@ -2,8 +2,8 @@
 
 (for ((i (read)))
   (let* ((n (read))
-        (k (read))
-        (times (for/list ((_ n)) (read))))
+         (k (read))
+         (times (for/list ((_ n)) (read))))
     (if (> k (length (filter (compose not positive?) times)))
         (displayln "YES")
         (displayln "NO"))))
