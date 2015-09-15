b36d457d8c302f3005605ccdeff5f961e22e1491
Matthew Chan <matt@themattchan.com>
2015-09-15 00:00:53 -0700

    time

---
+++ algo/warmup/time_convert.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	char time[10];
+	fgets(time, 10, stdin);
+	char AorP = time[8];
+	time[8] = '\0';
+
+	if (time[0] == '1' && time[1] == '2') {
+		if (AorP == 'A') {
+			time[0] = '0';
+			time[1] = '0';
+		}
+	} else if (AorP == 'P') {
+		time[0] += 1;
+		time[1] += 2;
+	}
+
+	printf("%s",time);
+    return 0;
+}
