c60cb62001c1694215fbadc56c061b2adad1ff41
Matthew Chan <matt@themattchan.com>
2015-09-14 19:23:57 -0700

    simpl

--- fp/intro/EvalEx.hs
+++ fp/intro/EvalEx.hs
@@ -1,7 +1,7 @@
 solve :: Double -> Double
-solve x = (1+) $ sum (e x)
-    where
-        e x = [(x**i)/(product [1..i]) | i <-[1..9]]
+solve  = (1+) . sum . e
+  where e x = [(x**i)/(product [1..i]) | i <- [1..9]]
 
 main :: IO ()
-main = getContents >>= mapM_ print. map solve. map (read::String->Double). tail. words
+main = getContents >>=
+       mapM_ print . map (solve . read) . tail . words
