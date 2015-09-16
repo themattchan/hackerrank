c41b00f1876cc7628a5e83f19aa7ec0328c3a85c
Matthew Chan <matt@themattchan.com>
2015-09-16 15:11:20 -0700

    some python

--- py/fp/validate_email.py
+++ py/fp/validate_email.py
@@ -5,7 +5,7 @@ def isValid(email):
     if len(e) != 3 or len(filter(lambda s: s, e)) != 3: return False
     u = re.search(r'[^\-\_\A-Za-z0-9]+',e[0]) == None
     w = re.search(r'[^\A-Za-z0-9]+',e[1]) == None
-    x = len(e[2]) >= 1 and len(e[2]) <= 3 and (re.search(r'[^\A-Za-z]+',e[2]) == None)
+    x = len(e[2]) >= 1 and len(e[2]) <= 3 and re.search(r'[^\A-Za-z]+',e[2]) == None
     return u and w and x
 
 emails = [raw_input() for i in range(input())]
