b64087bcd64bc07477ff43a24bc7ad7778165c4d
Matthew Chan <matt@themattchan.com>
2015-09-15 18:22:17 -0700

    simpl

--- fp/recursion/prefix.hs
+++ fp/recursion/prefix.hs
@@ -1,22 +1,20 @@
 module PrefixCompression where
 
-splitWithPrefix :: String -> String -> (String, String, String)
+splitWithPrefix :: String -> String -> [String]
 splitWithPrefix  = go []
   where
-    go as xs     []     = (reverse as, xs, [])
-    go as []     ys     = (reverse as, [], ys)
+    go as xs     []     = [reverse as, xs, []]
+    go as []     ys     = [reverse as, [], ys]
     go as (x:xs) (y:ys) = if x == y then go (x:as) xs ys
-                          else (reverse as, x:xs, y:ys)
+                          else [reverse as, x:xs, y:ys]
 
-printWithLength :: String -> IO ()
-printWithLength ss = let l = length ss in
-  putStrLn $ (show l) ++ " " ++ ss
+printWithLength :: String -> String
+printWithLength ss = let l = length ss
+                     in (show l) ++ " " ++ ss
 
 main :: IO ()
 main = do
   s1 <- getLine
   s2 <- getLine
-  let (prefix, ss1, ss2) = splitWithPrefix s1 s2
-  printWithLength prefix
-  printWithLength ss1
-  printWithLength ss2
+  mapM_ (putStrLn . printWithLength) $
+    splitWithPrefix s1 s2
