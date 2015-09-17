092dd7ea71d9e8ea5ec60da6a8f0187294c7d60e
Matthew Chan <matt@themattchan.com>
2015-09-16 18:26:31 -0700

    some python

---
+++ py/fp/maximise.py
@@ -0,0 +1,9 @@
+from itertools import *
+
+# observe that
+# [f(x%m)]%m == [f(x)]%m
+# [f(x%m)%m + f(y%m)%m +...]%m == [f(x%m)+f(y%m)+...]%m == [f(x)+f(y)+...]%m
+
+(k,m) = tuple(map(int, raw_input().split()))
+lists = [set(map(lambda x: int(x)%m,raw_input().split()[1:])) for _ in range(k)]
+print max([sum(map(lambda x:x*x,comb))%m for comb in product(*lists)])
