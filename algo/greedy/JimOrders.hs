e3fb0c4e626a579c18b24b702589c33f92777f7d
Matthew Chan <matt@themattchan.com>
2015-12-23 20:34:25 +0800

    more greedy

--- algo/greedy/JimOrders.hs
+++ algo/greedy/JimOrders.hs
@@ -1,12 +1,13 @@
+#!/usr/bin/env runhaskell
+
 import Control.Monad
 import Data.List
-
-
 
 main :: IO ()
 main = do
   n <- readLn
   os <- forM [1..n] $ \i -> do
     [t,d] <- liftM (map read . words) getLine
-    return (i,t,d)
-  return ()
+    return (i,t+d)
+  let ss = map fst $ sortOn snd os
+  putStrLn $ unwords (map show ss)
