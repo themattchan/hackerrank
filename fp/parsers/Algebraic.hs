6ef5a7a135d847ba46e6d0b0e80dbdec1c5a7c70
Matthew Chan <matt@themattchan.com>
2016-05-20 02:26:43 -0700

    parser

--- fp/parsers/Algebraic.hs
+++ fp/parsers/Algebraic.hs
@@ -3,3 +3,12 @@ import Text.Parsec.String
 import Text.Parsec.Expr
 import Text.Parsec.Token
 import Text.Parsec.Language
+
+data Expr =
+    Var Char
+  | Num Int
+  | Mul Expr Expr
+  | Div Expr Expr
+  | Sub Expr Expr
+  | Add Expr Expr
+  | Pow Expr Expr
