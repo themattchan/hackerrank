6c6bf1814cd6a5c83723d886681434b36c8782a7
Matthew Chan <matt@themattchan.com>
2016-05-20 02:33:05 -0700

    parser

--- fp/parsers/Algebraic.hs
+++ fp/parsers/Algebraic.hs
@@ -12,4 +12,16 @@ data Expr =
   | Sub Expr Expr
   | Add Expr Expr
   | Pow Expr Expr
-  deriving (Eq, Show)
+  deriving (Eq)
+
+instance Show Expr where
+  show expr = case expr of
+    Var x     -> show x
+    Num n     -> show n
+    Mul e1 e2 -> undefined
+    Div e1 e2 -> undefined
+    Sub e1 e2 -> show e1 ++ " - " ++ show e2
+    Add e1 e2 -> show e1 ++ " + " ++ show e2
+    Pow e1 e2 -> show e1 ++ "^" ++ show e2
+
+simpl :: Expr -> Expr
