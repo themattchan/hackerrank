88c87430297581cab1459995917093557383e7f8
Matthew Chan <matt@themattchan.com>
2016-05-21 00:46:41 -0700

    malloc

---
+++ lang/cpp/VariableSizedArrays.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#define foreach(var,until) for(int var = 0; var < until; var++)
+#define SIZE 100000
+
+using namespace std;
+
+int withVectors()
+{
+	int N,Q;
+	cin >> N >> Q;
+	vector<vector<int>> v(N);
+
+	foreach (i, N) {
+		int k;
+		cin >> k;
+		v[i].resize(k);
+		foreach (j, k) {
+			cin >> v[i][j];
+		}
+	}
+
+	foreach (i, Q) {
+		int a,b;
+		cin >> a >> b;
+		cout << v[a][b] << endl;
+	}
+
+	return 0;
+}
+
+int withMalloc()
+{
+	int N,Q;
+	cin >> N >> Q;
+	int** v = new int*[N];
+
+	foreach (i, N) {
+		int k;
+		cin >> k;
+		int* vi = new int[k];
+		foreach (j, k) {
+			cin >> vi[j];
+		}
+		v[i] = vi;
+	}
+
+	foreach (i, Q) {
+		int a,b;
+		cin >> a >> b;
+		cout << v[a][b] << endl;
+	}
+
+	foreach (i, N) {
+		delete[] v[i];
+	}
+	delete[] v;
+
+	return 0;
+}
+
+int main()
+{
+	withVectors();
+	withMalloc();
+	return 0;
+}
