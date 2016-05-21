41ad49fc64b5a4935d3f01da7dfac840b21a6dd6
Matthew Chan <matt@themattchan.com>
2016-05-21 03:23:42 -0700

    push

---
+++ distributed/uds_echo_server.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ev.h>
+
+#include <errno.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <unistd.h>
+/*
+  Write your code here
+  This function is called everytime a new connection is accepted by the server
+
+  Use this function to write data to socket
+  void write_string_to_socket(int sock_descriptor, char* message, uint32_t length);
+
+  Use this function to read data from socket
+  void read_string_from_socket(int sock_descriptor, char** message, uint32_t *length);
+*/
+
+void * process_client_connection(void * ptr)
+{
+	connection_t *conn;
+
+	if (!ptr) pthread_exit(0);
+	conn = (connection_t *)ptr;
+
+	printf("Connection received\n");
+
+	int terminate_client = 0;
+	do
+	{
+		char *message = NULL;
+		uint32_t message_length = 0;
+
+		/* read message */
+		read_string_from_socket(conn->sock, &message, &message_length);
+		printf("Received = %s\n", message);
+
+		/* End of operation on this clinet */
+		if (strcmp(message, "END") == 0)
+			terminate_client = 1;
+		else write_string_to_socket(conn->sock, message, message_length);
+
+		free(message);
+
+	} while (!terminate_client);
+
+	/* close socket and clean up */
+	printf("Closing client on socket %d\n", conn->sock);
+	close(conn->sock);
+	free(conn);
+	pthread_exit(0);
+}
