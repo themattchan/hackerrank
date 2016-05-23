854481947a50205337f0ab9ffe0dbe34acae584b
Matthew Chan <matt@themattchan.com>
2016-05-23 03:34:30 -0700

    done

--- algo/strings/attribute-parser.rkt
+++ algo/strings/attribute-parser.rkt
@@ -4,23 +4,28 @@
 
 (define parse-query
   (lambda (s)
-    (define spl (string-split s #rx"\\.|~"))
-    (define-values (path attrib)
-      (split-at spl (sub1 (length spl))))
-    (append
-     (map string->symbol path)
-     (list (string->keyword (car attrib))))))
+    (define spl (string-split s "~"))
+    (define path (car spl))
+    (define attr (if (= 2 (length spl)) (cdr spl) '()))
+
+    `(root
+      ,@(map string->symbol (string-split path "."))
+      ,@(map string->keyword attr))))
+
+(define (read-xml lines)
+  (string->xexpr
+          (apply string-append
+                 `("<root>"
+                 ,@(for/list ((i (range 0 lines)))
+                           (read-line))
+                 "</root>"))))
 
 (let ((lines (read))
       (queries (read)))
   (read-line)   ; clear trailing newline gunk
-  (let ((doc
-         (string->xexpr
-          (apply string-append
-                 (for/list ((i (range 0 lines)))
-                           (read-line))))))
+  (let ((doc (read-xml lines)))
     (for ((_ (range 0 queries)))
-      (let ((found (se-path* (parse-query (read-line)) doc)))
-      (if found
-          (displayln found)
-          (displayln "Not Found!"))))))
+         (let ((found (se-path* (parse-query (read-line)) doc)))
+           (if found
+               (displayln found)
+               (displayln "Not Found!"))))))
