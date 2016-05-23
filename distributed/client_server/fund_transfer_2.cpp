8ad7c725f88bdec3950608eb3cf2ee47623b4b5f
Matthew Chan <matt@themattchan.com>
2016-05-23 15:46:06 -0700

    fucking floating point

--- distributed/client_server/fund_transfer_2.cpp
+++ distributed/client_server/fund_transfer_2.cpp
@@ -6,6 +6,7 @@
 #include <resolv.h>
 #include <unistd.h>
 
+#include <cmath>
 #include <iostream>
 #include <fstream>
 #include <stack>
@@ -25,7 +26,7 @@ struct Node {
 	int transmit_prob;
 	int parent;
 };
-Node** nodes;
+Node* nodes;
 
 /*
 This function is called only once before any client connection is accepted by the server.
@@ -38,18 +39,17 @@ void init_server()
 	int N;
 	fscanf(topo, "%d", &N);
 
-	nodes = (Node**) malloc((N+1) * sizeof(Node*));
+	nodes = (Node*) malloc((N+1) * sizeof(Node));
 	for (int i = 1; i <= N; i++) {
-		Node* n = new Node();
-		n->parent = -1;
-		nodes[i] = n;
+		nodes[i].parent = -1;
 	}
 
 	for (int i = 0; i < N; i++) {
 		int parent, child, transmit_prob;
 		char comma;
 		fscanf(topo, "%d%c%d%c%d", &parent, &comma, &child, &comma, &transmit_prob);
-		nodes[child]->parent = parent;
+		nodes[child].parent = parent;
+		nodes[child].transmit_prob = transmit_prob;
 	}
 
 	fclose(topo);
@@ -57,7 +57,64 @@ void init_server()
 
 bool is_root(int node)
 {
-	return nodes[node]->parent == -1;
+	return nodes[node].parent == -1;
+}
+
+int ipow(int base, int exp)
+{
+    int result = 1;
+    while (exp)
+    {
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        base *= base;
+    }
+
+    return result;
+}
+
+double success_prob(int src, int dst)
+{
+	if (src == dst) return 1.00;
+
+	int src_node = src;
+	int dst_node = dst;
+
+	stack<int> src_path;
+	stack<int> dst_path;
+
+	while (!is_root(src_node)) {
+		src_path.push(src_node);
+		src_node = nodes[src_node].parent;
+	}
+
+	while (!is_root(dst_node)) {
+		dst_path.push(dst_node);
+		dst_node = nodes[dst_node].parent;
+	}
+
+	while (!src_path.empty() && !dst_path.empty() &&
+		   src_path.top() == dst_path.top()) {
+		src_path.pop();
+		dst_path.pop();
+	}
+
+	int pow = src_path.size() + dst_path.size();
+
+	int prob = 1;
+	while (src_path.size() > 0) {
+		int n = src_path.top();
+		src_path.pop();
+		prob *= nodes[n].transmit_prob;
+	}
+	while (dst_path.size() > 0) {
+		int n = dst_path.top();
+		dst_path.pop();
+		prob *= nodes[n].transmit_prob;
+	}
+
+	return ((double) prob / (double) ipow(100,pow));
 }
 
 /*
@@ -92,9 +149,9 @@ void * process_client_connection(void * ptr)
 		if (strcmp(message, "END") != 0) {
 			sscanf(message,"%d%c%d%c%lf", &src, &comma, &dst, &comma, &threshold);
 
-			bool path = path_len(src,dst) <= hops;
-			const char *reply = path ? "YES" : "NO";
-			uint32_t reply_len = path ? 3 : 2;
+			bool success = success_prob(src,dst) >= pow(10.0,threshold);
+			const char *reply = success ? "YES" : "NO";
+			uint32_t reply_len = success ? 3 : 2;
 
 			write_string_to_socket(conn->sock, reply, reply_len);
 		} else {
@@ -108,7 +165,7 @@ void * process_client_connection(void * ptr)
 
 	/* close socket and clean up */
 	printf("Closing client on socket %d\n", conn->sock);
-	 fflush(stdout);
+	fflush(stdout);
 	close(conn->sock);
 	free(conn);
 	pthread_exit(0);
