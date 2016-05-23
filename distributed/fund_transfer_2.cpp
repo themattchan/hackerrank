cffda813aa882f3f6afbd9692f2d1dd11278d5ac
Matthew Chan <matt@themattchan.com>
2016-05-23 00:21:53 -0700

    blargh

--- distributed/fund_transfer_2.cpp
+++ distributed/fund_transfer_2.cpp
@@ -66,26 +66,26 @@ This function is called everytime a new connection is accepted by the server
 */
 void * process_client_connection(void * ptr)
 {
-    connection_t * conn;
+	connection_t * conn;
 
-    if (!ptr) pthread_exit(0);
-    conn = (connection_t *)ptr;
+	if (!ptr) pthread_exit(0);
+	conn = (connection_t *)ptr;
 
-    printf("Connection received\n");
-     fflush(stdout);
+	printf("Connection received\n");
+	 fflush(stdout);
 
-    int terminate_client = 0;
-    do
-    {
-        /* read length of message */
-        char *message = NULL;
-        uint32_t message_length = 0;
+	int terminate_client = 0;
+	do
+	{
+		/* read length of message */
+		char *message = NULL;
+		uint32_t message_length = 0;
 
 		int src,dst;
 		double threshold;
 		char comma;
 
-       	/* read message */
+		/* read message */
 		read_string_from_socket(conn->sock, &message, &message_length);
 		printf("Received = %s\n", message);
 
@@ -102,14 +102,14 @@ void * process_client_connection(void * ptr)
 			terminate_client = 1;
 		}
 
-        free(message);
+		free(message);
 
-    } while(!terminate_client);
+	} while(!terminate_client);
 
-    /* close socket and clean up */
-    printf("Closing client on socket %d\n", conn->sock);
-     fflush(stdout);
-    close(conn->sock);
-    free(conn);
-    pthread_exit(0);
+	/* close socket and clean up */
+	printf("Closing client on socket %d\n", conn->sock);
+	 fflush(stdout);
+	close(conn->sock);
+	free(conn);
+	pthread_exit(0);
 }
