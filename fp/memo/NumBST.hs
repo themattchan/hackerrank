78e646d543b4d2f312b55dce8fdb809e85e5c9d9
Matthew Chan <matt@themattchan.com>
2016-09-09 01:28:30 -0700

    catalan numbers

---
+++ fp/memo/NumBST.hs
@@ -0,0 +1,17 @@
+-- Catalan Numbers
+import Control.Monad
+
+-- https://en.wikipedia.org/wiki/Catalan_number
+catalan :: Integer -> Integer
+catalan n = ((2*n) `choose` n) `div` (n+1) `mod` bigNumber
+  where
+    choose n 0 = 1
+    choose 0 k = 0
+    choose n k = choose (n-1) (k-1) * n `div` k
+
+    bigNumber = (10^8 +7)
+
+
+main :: IO ()
+main = readLn >>= \n -> replicateM_ n $
+   readLn >>= \x -> print (catalan x)
