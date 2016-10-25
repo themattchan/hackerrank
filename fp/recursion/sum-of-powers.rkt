0b3a477829432ab2bf5db72bfd0de022b37c43f1
Matthew Chan <matt@themattchan.com>
2016-10-24 21:45:34 -0700

    sum of powers

--- fp/recursion/sum-of-powers.rkt
+++ fp/recursion/sum-of-powers.rkt
@@ -3,13 +3,13 @@
 (define (sum-of-powers goal power)
   (define (go goal m)
     (let ((m^p (expt m power)))
-    (cond
-      ((= goal 0) 1)
-      ((< goal 0) 0)
-      ((< goal m^p) 0)
-      (else
-       (+ (go goal (add1 m))
-          (go (- goal m^p) (add1 m)))))))
+      (cond
+        ((= goal 0) 1)
+        ((< goal 0) 0)
+        ((< goal m^p) 0)
+        (else
+         (+ (go goal (add1 m))
+            (go (- goal m^p) (add1 m)))))))
 
   (go goal 1))
 
