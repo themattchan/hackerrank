f3626a3fa0295786619ef05e8563cfdd8e74332b
Matthew Chan <matt@themattchan.com>
2016-11-16 15:31:13 -0800

    dsfkdsfjl

--- fp/misc/missing-numbers.rkt
+++ fp/misc/missing-numbers.rkt
@@ -1,16 +1,21 @@
 #lang racket
 
-(define ns (make-vector 100 (cons 0 0)))
+(define ns (make-vector 100 (cons null 0)))
 
 (define (do-row action)
   (for/last ((_ (read)))
-    (let*-values (((q r) (quotient/remainder (read) 100))
-                 ((n) (vector-ref ns r)))
-      (vector-set! ns r (cons (* 100 q) (action (cdr n)))))))
+    (let* ((v (read))
+           (r (modulo v 100))
+           (n (vector-ref ns r)))
+      (vector-set! ns r (cons (if (null? (car n))
+                                  (- v r)
+                                  (car n))
+                              (action (cdr n)))))))
 
 (let ((a (do-row add1))
       (b (do-row sub1)))
-  (for (((n i) (in-indexed ns))
-             #:when (< (cdr n) 0))
-    (printf "~a " (+ (car n) i))))
-             
+  (for-each (λ (e) (printf "~a " e))
+            (sort
+             (for/list (((n i) (in-indexed ns))
+                        #:when (< (cdr n) 0))
+               (+ (car n) i)) <)))
