2c58b5caa10dd4fe6689ce667b4b3f4e8767850e
Matthew Chan <matt@themattchan.com>
2017-03-10 15:33:08 -0800

    fdsfjdslfj

--- fp/interp/while/While.hs
+++ fp/interp/while/While.hs
@@ -1,15 +1,21 @@
 module While where
+import Prelude hiding (lookup)
 import Control.Applicative hiding (empty, (<|>))
-import qualified Data.List as Lst ((\\))
-import Data.Map hiding (foldl, foldr)
+import Control.Monad
 import Control.Monad.State hiding (when)
+
+import qualified Data.Map as M
+
 import Text.Parsec hiding (State, between)
 import Text.Parsec.Combinator hiding (between)
 import Text.Parsec.Char
 import Text.Parsec.String
 
--- TYPES
+--------------------------------------------------------------------------------
+-- * Embedding
+
 type Variable = String
+type Value = Int
 
 data Stmt
   = Assign Variable AExpr
@@ -31,7 +37,7 @@ data RelOp
   deriving (Show)
 
 data AExpr
-  = Val Int
+  = Val Value
   | Var Variable
   | AApp ArithOp AExpr AExpr
   deriving (Show)
@@ -42,8 +48,113 @@ data BExpr
   | BCmp RelOp AExpr AExpr
   deriving (Show)
 
--- This is the state of the interpreter
-type Store = Map Variable Value
+--------------------------------------------------------------------------------
+-- * Parser
 
-evalBExpr :: BExpr -> State Store Bool
-evalBExpr TRUE = return
+eatSpaces :: Parser a -> Parser a
+eatSpaces = (spaces *>) . (<* spaces)
+
+parseVar :: Parser Variable
+parseVar = many1 letter
+
+
+-- parser :: Parser Expr
+-- parser = buildExpressionParser optable factor
+--   where
+--     binary name fun = Infix  (eatSpaces (string name) >> return fun) AssocRight
+--     prefix name fun = Prefix (eatSpaces (string name) >> return fun)
+--     -- tightest binding first
+--     optable = [ [prefix "-" Neg, prefix "+" id]
+--               , [binary "*" Mul, binary "/" Div, binary "^" Pow]
+--               , [binary "+" Add, binary "-" Sub]
+--               ]
+
+-- eatSpaces :: Parser a -> Parser a
+-- eatSpaces = (spaces *>) . (<* spaces)
+
+-- factor :: Parser Expr
+-- factor = try paren <|> try implicitMul <|> try num <|> var
+--   where
+--     paren       = between (char '(') (char ')') (eatSpaces parser)
+--     implicitMul = (Mul <$> num <*> var) <|> (Mul <$> num <*> paren)
+--     num         = Num . read <$> eatSpaces (many1 digit)
+--     var         = Var <$> eatSpaces lower
+
+
+-- parseAOp :: Parser ArithOp
+-- parseAOp =  char '+' *> pure Plus
+--         <|> char '-' *> pure Sub
+--         <|> char '*' *> pure Mul
+--         <|> char '/' *> pure Div
+
+-- parseBOp :: Parser BoolOp
+-- parseBOp =  string "and" *> pure And
+--         <|> string "or"  *> pure Or
+
+-- parseROp :: Parser RelOp
+-- parseROp = undefined
+
+-- parseAExpr :: Parser AExpr
+-- parseAExpr = undefined
+
+-- parseBExpr :: Parser BExpr
+-- parseBExpr = undefined
+
+-- parseProg :: Parser Stmt
+-- parseProg = undefined
+
+--------------------------------------------------------------------------------
+-- * Interpreter
+
+-- This is the state of the interpreter
+type Store = M.Map Variable Value
+type Machine a = State Store a
+
+runMachine :: Machine a -> Store
+runMachine = flip execState M.empty
+
+lookup :: Variable -> Machine Value
+lookup = gets . flip (M.!)
+
+assign :: Variable -> Value -> Machine ()
+assign v e = modify (M.insert v e)
+
+evalAOp :: ArithOp -> (Int -> Int -> Int)
+evalAOp Plus = (+)
+evalAOp Sub = (-)
+evalAOp Mul = (*)
+evalAOp Div = div
+
+evalBOp :: BoolOp -> (Bool -> Bool -> Bool)
+evalBOp And = (&&)
+evalBOp Or  = (||)
+
+evalROp :: RelOp -> (Int -> Int -> Bool)
+evalROp Lt = (<)
+evalROp Gt = (>)
+
+evalArith :: AExpr -> Machine Int
+evalArith (Val i)         = return i
+evalArith (Var v)         = lookup v
+evalArith (AApp op e1 e2) = evalAOp op <$> evalArith e1 <*> evalArith e2
+
+evalBool :: BExpr -> Machine Bool
+evalBool TRUE               = return True
+evalBool FALSE              = return False
+evalBool (BApp bop be1 be2) = evalBOp bop <$> evalBool be1  <*> evalBool be2
+evalBool (BCmp rop ae1 ae2) = evalROp rop <$> evalArith ae1 <*> evalArith ae2
+
+evalProg :: Stmt -> Machine ()
+evalProg (Assign v e) = evalArith e >>= assign v
+evalProg (Seq e1 e2)  = evalProg e1 >> evalProg e2
+evalProg (If b e1 e2) = do
+  b' <- evalBool b
+  if b' then evalProg e1
+        else evalProg e2
+evalProg (While b e)  = do
+  b' <- evalBool b
+  if b' then evalProg e >> evalProg (While b e)
+        else return ()
+
+main :: IO ()
+main = return ()
--- fp/parsers/Algebraic.hs
+++ fp/parsers/Algebraic.hs
@@ -1,35 +1,104 @@
+-- | Simplify the algebraic expressions
+{-
+
+10x + 2x - (3x + 6)/3
+18*(2x+2) - 5
+((9x + 81)/3 + 27)/3  - 2x
+18x + (12x + 10)*(2x+4)/2 - 5x
+(2x+5) * (x*(9x + 81)/3 + 27)/(1+1+1)  - 2x
+(2x+5) * (x*(9x^3 + 81)/3 + 27)/(1+1+1)  - 2x
+
+Sample Output
+
+11x - 2
+36x + 31
+-x + 18
+12x^2 + 47x + 20
+2x^3 + 23x^2 + 61x + 45
+2x^5 + 5x^4 + 18x^2 + 61x + 45
+-}
+
 import Text.Parsec
 import Text.Parsec.String
 import Text.Parsec.Expr
 import Text.Parsec.Token
 import Text.Parsec.Language
 
-data Expr =
-    Var Char
+data Expr
+  = Var Char
   | Num Int
   | Mul Expr Expr
   | Div Expr Expr
   | Sub Expr Expr
   | Add Expr Expr
   | Pow Expr Expr
-  deriving (Eq)
+  | Neg Expr
+  deriving (Eq, Show)
 
-instance Show Expr where
-  show expr = case expr of
-    Var x     -> show x
-    Num n     -> show n
-    Mul e1 e2 -> undefined
-    Div e1 e2 -> undefined
-    Sub e1 e2 -> show e1 ++ " - " ++ show e2
-    Add e1 e2 -> show e1 ++ " + " ++ show e2
-    Pow e1 e2 -> show e1 ++ "^" ++ show e2
+-- instance Show Expr where
+--   show e = case e of
+--     Var x     -> [x]
+--     Num n     -> show n
+--     Mul e1 e2 -> show e1 ++ show e2
+--     Div e1 e2 -> show e1 ++ "/"   ++ show e2
+--     Sub e1 e2 -> show e1 ++ " - " ++ show e2
+--     Add e1 e2 -> show e1 ++ " + " ++ show e2
+--     Pow e1 e2 -> show e1 ++ "^"   ++ show e2
+--     Neg e1    -> "-" ++ show e1
 
-simpl :: Expr -> Expr
-simpl e = case e of
-  Var c     -> e
-  Num i     -> e
-  Mul e1 e2 ->
-  Div e1 e2 ->
-  Sub e1 e2 ->
-  Add e1 e2 ->
-  Pow e1 e2 ->
+parser :: Parser Expr
+parser = buildExpressionParser optable factor
+  where
+    binary name fun = Infix  (eatSpaces (string name) >> return fun) AssocRight
+    prefix name fun = Prefix (eatSpaces (string name) >> return fun)
+    -- tightest binding first
+    optable = [ [prefix "-" Neg, prefix "+" id]
+              , [binary "*" Mul, binary "/" Div, binary "^" Pow]
+              , [binary "+" Add, binary "-" Sub]
+              ]
+
+eatSpaces :: Parser a -> Parser a
+eatSpaces = (spaces *>) . (<* spaces)
+
+factor :: Parser Expr
+factor = try paren <|> try implicitMul <|> try num <|> var
+  where
+    paren       = between (char '(') (char ')') (eatSpaces parser)
+    implicitMul = (Mul <$> num <*> var) <|> (Mul <$> num <*> paren)
+    num         = Num . read <$> eatSpaces (many1 digit)
+    var         = Var <$> eatSpaces lower
+
+-- | Evaluate purely numeric expressions
+eval :: Expr -> Maybe Int
+eval (Var _)     = Nothing
+eval (Num n)     = Just n
+eval (Mul e1 e2) = (*) <$> eval e1 <*> eval e2
+eval (Div e1 e2) = div <$> eval e1 <*> eval e2
+eval (Sub e1 e2) = (-) <$> eval e1 <*> eval e2
+eval (Add e1 e2) = (+) <$> eval e1 <*> eval e2
+eval (Pow e1 e2) = (^) <$> eval e2 <*> eval e2
+eval (Neg e)     = (* (-1)) <$> eval e
+
+-- simplConst :: Expr -> Expr
+-- simplConst e = case e of
+--   Var c     -> e
+--   Num i     -> e
+--   Mul (Num e1) (Num e2) -> Num (e1 * e2)
+--   Mul (Num e1) (Div e2 (Num e3)) -> Div e2 (Num (e1 `div` e3))
+--   Div (Num e1) (Num e2) -> Num (e1 `div` e2)
+--   Sub e1 e2 ->
+--   Add e1 e2 ->
+--   Pow e1 e2 ->
+--   Pos e1    -> e1
+--   Neg (Num e1) -> Num (e1 * (-1))
+doParse :: String -> Maybe Expr
+doParse = hush . parse parser ""
+  where hush (Right e) = Just e
+        hush _         = Nothing
+
+main :: IO ()
+main = do
+  s <- getLine
+  print $ parse parser "" s
+--  print e
+--  print . eval $ e
