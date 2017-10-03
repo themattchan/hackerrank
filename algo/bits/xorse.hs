a7aba96f14e670eca9a6d1ec54d9800b6f4f6073
Matthew Chan <matt@awakenetworks.com>
2017-10-03 03:11:08 -0700

    optimised but still slow... sigh

--- algo/bits/xorse.hs
+++ algo/bits/xorse.hs
@@ -1,4 +1,5 @@
-import Data.Bits (xor)
+{-# LANGUAGE BangPatterns #-}
+import Data.Bits
 import Data.List(foldl')
 import Control.Monad(replicateM_)
 -- A0 = 0
@@ -10,11 +11,24 @@ import Control.Monad(replicateM_)
 -- Ri is done once, Ri-1 done twice, Ri-2 done thrice...
 -- Counting from Ri, drop every second number.
 
+-- remember from game theory that xor of [1..n] is given by
+-- n mod 4 = 0 -> n
+-- n mod 4 = 1 -> 1
+-- n mod 4 = 2 -> n+1
+-- n mod 4 = 3 -> 0
+xorTo :: Integer -> Integer
+xorTo n = case n `mod` 4 of
+            0 -> n
+            1 -> 1
+            2 -> n+1
+            3 -> 0
+            _ -> error "impossible"
+
 
 xorRange :: Integer -> Integer -> Integer
 xorRange lo hi = belows `xor` bounds
   where
-    belows | (hi-lo+1) `mod` 2 == 1 = foldl' xor 0 [1..lo-1]
+    belows | (hi-lo+1) `mod` 2 == 1 = xorTo (lo-1)
            | otherwise = 0
     bounds = foldl' xor 0 [hi, hi-2 .. lo]
 
