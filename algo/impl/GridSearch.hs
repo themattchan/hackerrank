7aacd87efe4effbff47936662e9d0042fe01769a
Matthew Chan <matt@awakenetworks.com>
2017-10-06 20:51:32 -0700

    foo

--- algo/impl/GridSearch.hs
+++ algo/impl/GridSearch.hs
@@ -6,20 +6,19 @@ main :: IO ()
 main = do
   t <- readLn
   replicateM_ t $ do
-    [big, small] <- replicateM 2 $ do
-      (r:_) <- map read . words <$> getLine
-      replicateM r getLine
-    putStrLn $ bool "NO" "YES" (search small big)
-
+      [big, small] <- replicateM 2 $ do
+        r <- read . head .  words <$> getLine
+        replicateM r getLine
+      putStrLn $ bool "NO" "YES" (search small big)
 
 search :: [String] -> [String] -> Bool
 search needle@(n:ns) =
-  any (\(h:hs) -> any (\i -> matchRest ns (map (drop i) hs)) (findStarts n h))
-  . init . tails
+  any (\(h:hs) -> any (\i -> matchRest ns (map (drop i) hs)) (findStarts n h)) . init . tails
   where
-    findStarts targ s = [ n | (n,t) <- zip [0..] (tails s)
-                            , targ `isPrefixOf` t
-                        ]
+    findStarts targ s =
+      [ n
+      | (n,t) <- zip [0..] (tails s)
+      , targ `isPrefixOf` t
+      ]
 
-    matchRest ns hs
-      = length hs >= length ns && and (zipWith isPrefixOf ns hs)
+    matchRest ns hs = length hs >= length ns && and (zipWith isPrefixOf ns hs)
