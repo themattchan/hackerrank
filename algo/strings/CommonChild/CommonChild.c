3c3459e50d2c30cc99409b9ab955c99c8fd44c79
Matthew Chan <matt@themattchan.com>
2018-07-23 01:48:49 -0700

    benchmark

--- algo/strings/CommonChild/CommonChild.c
+++ algo/strings/CommonChild/CommonChild.c
@@ -33,6 +33,7 @@ int lcs (char* xs, size_t lenxs, char* ys, size_t lenys) {
 
 // benchmark
 // on input05: ./CommonChildC  0.06s user 0.00s system 94% cpu 0.062 total
+// 6.2x faster than the fastest Haskell version (lcs5, 0.384)
 int main () {
 	char xs[10000];
 	char ys[10000];
