796aafd3b991a709989c7987a1c879973a0e55e0
Matthew Chan <matt@themattchan.com>
2018-07-23 02:16:24 -0700

    strong password

---
+++ algo/strings/strong_pass.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define NEED(x) ((x==0?1:0))
+#define MAX(x,y) (x<y?y:x)
+
+int main () {
+	int n;
+	scanf("%d\n", &n);
+	int len = 6;
+	int d = 0;
+	int l = 0;
+	int u = 0;
+	int p = 0;
+
+	char pass[n];
+	gets(pass);
+	char* cur = pass;
+	while (*cur) {
+		len--;
+		char c = *cur;
+		if (isdigit(c)) d++;
+		if (islower(c)) l++;
+		if (isupper(c)) u++;
+		if (ispunct(c)) p++;
+		cur++;
+		if (len <= 0 && d && l && u && p) break;
+	}
+
+	int need = MAX(len, NEED(d)+NEED(l)+NEED(u)+NEED(p));
+	printf("%d\n",need);
+
+}
