cecf988acb574250efb32577e06e2339e3b5a19b
Matthew Chan <matt@themattchan.com>
2019-05-04 02:47:58 -0700

    add it

--- fp/interp/Infer.hs
+++ fp/interp/Infer.hs
@@ -1,21 +1,14 @@
-import Prelude hiding (lookup)
-import Control.Arrow ((>>>))
-import Control.Applicative hiding (empty, (<|>))
 import Control.Monad
-import Control.Monad.State hiding (when)
-
+import Data.Either
+import qualified Text.Parsec
+import Text.Parsec hiding (runParser)
+import Text.Parsec.String(Parser(..))
+import Text.Parsec.Char
+import Text.Parsec.Combinator
 import qualified Data.Map as M
-import Data.List (foldl1)
-import Data.Functor (($>))
 
-import Text.Parsec hiding (State, between, spaces)
-import Text.Parsec.String (Parser)
-import Text.Parsec.Expr
-import qualified Text.Parsec.Token as Token
-import Text.Parsec.Language
-
-import Data.Semigroup
-
+runParser :: Parser a -> String -> Either Text.Parsec.ParseError a
+runParser p = Text.Parsec.runParser p () ""
 
 {-
 ident : [_A-Za-z][_A-Za-z0-9]*              // variable names
@@ -33,14 +26,47 @@ simpleExpr : '(' expr ')'
 paramList : { 0 or more expr seperated ", " }
 -}
 
-type Ident = String
-data Exp = Let Ident Exp Exp
-         | Fun [Ident] Exp
-         | Var Ident
-         | App Exp [Exp]
+data Expr = Let String Expr Expr
+          | Fun [String] Expr
+          | Id String
+          | Call Expr [Expr]
+
+parseIdent :: Parser String
+parseIdent = (:) <$>(try upper<|>try lower<|>try (char '_'))<*> many (try alphaNum<|>try (char '_'))
+
+parens = between (char '(') (char ')')
+brackets = between (char '[') (char ']')
+
+parseArgList = sepBy parseIdent (string " ")
+
+parseExpr :: Parser Expr
+parseExpr = parseLet <|> parseFun <|> parseSimple
+  where
+    parseLet = do
+      string "let "
+      i <- parseIdent
+      string " = "
+      e <- parseExpr
+      string " in "
+      b <- parseExpr
+      return $ Let i e b
+
+    parseFun = do
+      string "fun "
+      args <- parseArgList
+      string " -> "
+      e <- parseExpr
+      return $ Fun args e
+
+    parseSimple = parens parseExpr <|> (Id <$> parseIdent) <|> parseFuncall
+      where
+        parseFuncall = do
+          e <- parseSimple
+          args <- parens $ sepBy parseExpr (string ", ")
+          return $ Call e args
+
 
 {-
-
 ty : "() -> " ty                            // function without arguments
    | '(' tyList ") -> " ty                  // uncurry function
    | "forall[" argList "]" ty               // generic type
@@ -53,60 +79,55 @@ simpleTy : '(' ty ')'
          | ident
          | simpleTy '[' tyList ']'          // such as list[int]
 -}
-
-data TMono = TFunU [TMono] TMono
-           | TFunS TMono TMono
-           | TApp TMono [TMono]
-           | TV Ident
-           | Unit
-
-data TPoly = Forall [Ident] TPoly
-           | TM TMono
-
--- pTy :: Parser Ty
--- pTy =
-
--- algorithm j
-
-generalise :: TMono -> TPoly
-generalise ty = Forall (freevars ty) (TM ty)
-
-freevarsMono :: TMono -> [Ident]
-freevarsMono = go S.empty where
-  go (
+-- data Poly = Forall [TVar] Ty
+-- newtype TVar = TV String
+data Ty = [Ty] :=> Ty
+        | TCtor Ty [Ty]
+        | TId String
+        | TPoly [String] Ty
+        deriving (Eq, Show)
 
 
---newtype IM a = IM { Int -> (Int, a) }
-type TCtx = [(Ident, TPoly)]
+parseTy :: Parser Ty
+parseTy = try parseFun <|> try parsePoly <|> try parseCurriedFun <|> try parseSimpleTy
+  where
+    parseFun = do
+      argTys <- parens $ sepBy parseTy (string ", ")
+      string " -> "
+      retTy <- parseTy
+      return $ argTys :=> retTy
 
+    parseCurriedFun =  do
+      a <- parseSimpleTy
+      string " -> "
+      b <- parseTy
+      return $ [a] :=> b
 
-infer :: Exp -> IM TPoly
-infer (Let x e1 e2) =
+    parsePoly = do
+      string "forall"
+      args <- brackets parseArgList
+      space
+      ty <- parseTy
+      return $ TPoly args ty
 
-env0 =
-  ["head: forall[a] list[a] -> a"
-  ,"tail: forall[a] list[a] -> list[a]"
-  ,"nil: forall[a] list[a]"
-  ,"cons: forall[a] (a, list[a]) -> list[a]"
-  ,"cons_curry: forall[a] a -> list[a] -> list[a]"
-  ,"map: forall[a b] (a -> b, list[a]) -> list[b]"
-  ,"map_curry: forall[a b] (a -> b) -> list[a] -> list[b]"
-  ,"one: int"
-  ,"zero: int"
-  ,"succ: int -> int"
-  ,"plus: (int, int) -> int"
-  ,"eq: forall[a] (a, a) -> bool"
-  ,"eq_curry: forall[a] a -> a -> bool"
-  ,"not: bool -> bool"
-  ,"true: bool"
-  ,"false: bool"
-  ,"pair: forall[a b] (a, b) -> pair[a, b]"
-  ,"pair_curry: forall[a b] a -> b -> pair[a, b]"
-  ,"first: forall[a b] pair[a, b] -> a"
-  ,"second: forall[a b] pair[a, b] -> b"
-  ,"id: forall[a] a -> a"
-  ,"const: forall[a b] a -> b -> a"
-  ,"apply: forall[a b] (a -> b, a) -> b"
-  ,"apply_curry: forall[a b] (a -> b) -> a -> b"
-  ,"choose: forall[a] (a, a) -> a"
-  ,"choose_curry: forall[a] a -> a -> a"]
+    parseSimpleTy = try (parens parseTy) <|> (TId <$> parseIdent)<|> try parseTCtor
+      where
+        parseTCtor = do
+          ct <- parseSimpleTy
+          args <- brackets $ sepBy1 parseTy (string ", ")
+          return $ TCtor ct args
+
+type TEnv = M.Map String Ty
+
+initialTEnv :: TEnv
+initialTEnv = M.unions $ either mempty id $ runParser (go `sepEndBy` newline) env
+  where
+    go = do
+      s <- parseIdent
+      string ": "
+      ty <- parseTy
+      return (M.singleton s ty)
+
+    env = "head: forall[a] list[a] -> a\n\ntail: forall[a] list[a] -> list[a]\nnil: forall[a] list[a]\ncons: forall[a] (a, list[a]) -> list[a]\ncons_curry: forall[a] a -> list[a] -> list[a]\nmap: forall[a b] (a -> b, list[a]) -> list[b]\nmap_curry: forall[a b] (a -> b) -> list[a] -> list[b]\none: int\nzero: int\nsucc: int -> int\nplus: (int, int) -> int\neq: forall[a] (a, a) -> bool\neq_curry: forall[a] a -> a -> bool\nnot: bool -> bool\ntrue: bool\nfalse: bool\npair: forall[a b] (a, b) -> pair[a, b]\npair_curry: forall[a b] a -> b -> pair[a, b]\nfirst: forall[a b] pair[a, b] -> a\nsecond: forall[a b] pair[a, b] -> b\nid: forall[a] a -> a\nconst: forall[a b] a -> b -> a\napply: forall[a b] (a -> b, a) -> b\napply_curry: forall[a b] (a -> b) -> a -> b\nchoose: forall[a] (a, a) -> a\nchoose_curry: forall[a] a -> a -> a\n"
+
+main = return ()
